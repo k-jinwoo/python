497bfbecd342f58c99c9c8d84769169ac9e92400
kim_3838 <kim_3838@naver.com>
2021-04-28 17:10:30 +0900

    2021/04/28 파이썬 function 수업예제

---
+++ Ch05/5_1_Function.py
@@ -0,0 +1,70 @@
+"""
+날짜 : 2021/04/28
+이름 : 김진우
+내용 : 파이썬 함수 실습 교재 p114
+
+함수(function)
+ - 프로그래밍에서 일련의 로직 단위를 모듈로 만든 것
+ - 함수는 정의, 호출로 이루어진다
+"""
+
+# 함수 정의
+def f(x):
+    y = 2 * x + 3
+    return  y
+
+# 함수 호출
+r1 = f(1)
+r2 = f(2)
+r3 = f(3)
+
+print('r1 :', r1)
+print('r2 :', r2)
+print('r3 :', r3)
+
+# 함수 유형 1 - 매개변수 O, 리턴값 O
+def type1(x,y):
+    z = x + y
+    return z
+
+rs1 = type1(1,2)
+rs2 = type1(2,3)
+
+print('rs1 :', rs1)
+print('rs2 :', rs2)
+
+# 함수 유형 2 - 매개변수 O, 리턴값 X
+def type2(items):
+    total = 0
+
+    for item in items:
+        total += item
+
+    print('items 합 :', total)
+
+type2([1,2,3,4,5])
+type2((2,4,6,8,10))
+
+# 함수 유형 3 - 매개변수 X, 리턴값 O
+def type3():
+    total = 0
+
+    for i in range(11):
+        total += i
+    return total
+result = type3()
+print('result :', result)
+
+# 함수 유형 4 - 매개변수 X, 리턴값 X
+def type4():
+    print('type3 result :', type3())
+type4()
+
+# 확인 문제
+def gugudan(num):
+    print(num,'단')
+    for i in range(1, 10):
+        print('{} x {} = {}'.format(num, i, num*i))
+gugudan(2)
+gugudan(3)
+gugudan(5)
