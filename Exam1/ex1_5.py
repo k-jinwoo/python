2423bd29dde8bf326f2a34da9b92144e2832b101
kim_3838 <kim_3838@naver.com>
2021-04-30 16:05:08 +0900

    2021/04/30 파이썬 Exam1

---
+++ Exam1/ex1_5.py
@@ -0,0 +1,9 @@
+str = '12345'
+sum = 0
+
+for i in range(len(str)) :
+
+    num = int(str[i])
+    sum += num
+
+print('합 :', sum)
---
+++ Exam1/ex1_9.py
@@ -0,0 +1,13 @@
+dataset = [3,5,1,2,4]
+size = len(dataset)
+#print(len(dataset)) # 5
+for i in range(size -1) :
+    for j in range(i+1, size) :
+        if dataset[i] > dataset[j] :
+            tmp = dataset[i]
+            dataset[i] = dataset[j]
+            dataset[j] = tmp
+
+    print('%d Round : %s' % (i, dataset))
+
+print('Result :', dataset)
